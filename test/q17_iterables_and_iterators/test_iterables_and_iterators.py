2bbd3a53bb926fc4980df307d35c46fa046a88f4
DBT-Stewart <stewartprince.PM@diggibyte.com>
2025-06-09 21:45:51 +0530

    Q17 : Added a unit test file for the probability_of_a() function for testing.

--- test/q17_iterables_and_iterators/test_iterables_and_iterators.py
+++ test/q17_iterables_and_iterators/test_iterables_and_iterators.py
@@ -2,3 +2,27 @@ import sys
 import os
 import unittest
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
+import unittest
+from src.q17_iterables_and_iterators.util import probability_of_a
+from unittest.mock import patch
+from io import StringIO
+
+class TestProbabilityOfA(unittest.TestCase):
+
+    def test_basic_case(self):
+        self.assertEqual(probability_of_a(['a', 'b', 'c'], 2), 0.667)
+
+    def test_no_a(self):
+        self.assertEqual(probability_of_a(['b', 'c', 'd'], 2), 0.000)
+
+    def test_all_a(self):
+        self.assertEqual(probability_of_a(['a', 'a', 'a'], 2), 1.000)
+
+    def test_k_equals_n(self):
+        self.assertEqual(probability_of_a(['a', 'b'], 2), 1.000)
+
+    def test_k_greater_than_n(self):
+        self.assertEqual(probability_of_a(['a', 'b'], 3), 0.0)
+
+if __name__ == '__main__':
+    unittest.main()
